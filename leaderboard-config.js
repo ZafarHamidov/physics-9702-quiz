1e65cefddb7f7526a29742b114cad71837b2717d
Zafar <zafar@example.com>
2026-06-02 13:43:58 +0400

    Configure Firebase leaderboard

--- leaderboard-config.js
+++ leaderboard-config.js
@@ -2,11 +2,12 @@ window.LEADERBOARD_CONFIG = {
   collectionName: "physics_9702_w25_11",
   liveCollectionName: "physics_9702_w25_11_live",
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyBjxlp6QlgiIDVBCQ0V6JGAL8eTlYwSoRE",
+    authDomain: "physics-9702-quiz.firebaseapp.com",
+    projectId: "physics-9702-quiz",
+    storageBucket: "physics-9702-quiz.firebasestorage.app",
+    messagingSenderId: "1034947995419",
+    appId: "1:1034947995419:web:edafcdc7b58874467515be",
+    measurementId: "G-H1TGCG4X7G"
   }
 };
